f82f3a9c0ad46daa49dd42e67b31776757494d9d
gardian <gardian405@proton.me>
2024-12-09 15:17:51 +0500

    styles mob

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
 
   return (
     <div className="App">
-      <div style={{ position:"relative", height: "800px", width: "700px"  }}>
+      <div style={{ position:"relative", height: "95dvh", width: "100%"  }}>
         <MainContainer>
           <ChatContainer>
             <MessageList
